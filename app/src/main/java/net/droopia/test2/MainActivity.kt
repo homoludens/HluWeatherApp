d2b60119e25252821a198715b3f8673c11fbe843
Marko Djordjevic <marko@droopia.net>
2021-09-18 14:22:52 +0200

    initial commit of minimum web page parsing app, using code from  https://thetechstack.net/using-jsoup-with-kotlin-to-scrape-wiki-pages/

---
+++ app/src/main/java/net/droopia/test2/MainActivity.kt
@@ -0,0 +1,74 @@
+package net.droopia.test2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jsoup.Jsoup
+import net.droopia.test2.getWebPage
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getHtmlPage()
+    }
+
+    val wiki = "https://en.wikipedia.org"
+
+    fun getHtmlPage() {
+        Thread(Runnable {
+            val doc =
+                Jsoup.connect("$wiki/wiki/List_of_films_with_a_100%25_rating_on_Rotten_Tomatoes")
+                    .get()    // <1>
+            doc.select(".wikitable:first-of-type tr td:first-of-type a")    // <2>
+                .map { col -> col.attr("href") }    // <3>
+                .parallelStream()    // <4>
+                .map { extractMovieData(it) }    // <5>
+                .filter { it != null }
+                .forEach { println(it) }
+        }).start()
+    }
+
+    fun extractMovieData(url: String): Movie? { // <1>
+        val doc: Document
+        try {
+            doc = Jsoup.connect("$wiki$url").get()  // <2>
+        }catch (e: Exception){
+            return null
+        }
+
+        val movie = Movie() // <3>
+        doc.select(".infobox tr")   // <4>
+            .forEach { ele ->   // <5>
+                when {
+                    ele.getElementsByTag("th")?.hasClass("summary") ?: false -> {   // <6>
+                        movie.title = ele.getElementsByTag("th")?.text()
+                        println(movie.title)
+                    }
+                    else -> {
+                        val value: String? = if (ele.getElementsByTag("li").size > 1)
+                            ele.getElementsByTag("li")
+                                .map(Element::text)
+                                .filter(String::isNotEmpty)
+                                .joinToString(", ") else
+                            ele.getElementsByTag("td")?.first()?.text() // <7>
+
+                        when (ele.getElementsByTag("th")?.first()?.text()) {    // <8>
+                            "Directed by" -> movie.directedBy = value ?: ""
+                            "Produced by" -> movie.producedBy = value ?: ""
+                            "Written by" -> movie.writtenBy = value ?: ""
+                            "Starring" -> movie.starring = value ?: ""
+                            "Music by" -> movie.musicBy = value ?: ""
+                            "Release date" -> movie.releaseDate = value ?: ""
+                            "title" -> movie.title = value ?: ""
+                        }
+                    }
+                }
+            }
+        return movie
+    }
+
+
+}
---
+++ app/src/main/java/net/droopia/test2/getWebPage.kt
@@ -0,0 +1,62 @@
+package net.droopia.test2
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+class getWebPage {
+
+    val wiki = "https://en.wikipedia.org"
+
+    fun getHtmlPage() {
+        Thread(Runnable {
+            val doc =
+                Jsoup.connect("$wiki/wiki/List_of_films_with_a_100%25_rating_on_Rotten_Tomatoes")
+                    .get()    // <1>
+            doc.select(".wikitable:first-of-type tr td:first-of-type a")    // <2>
+                .map { col -> col.attr("href") }    // <3>
+                .parallelStream()    // <4>
+                .map { extractMovieData(it) }    // <5>
+                .filter { it != null }
+                .forEach { println(it) }
+        }).start()
+    }
+
+    fun extractMovieData(url: String): Movie? { // <1>
+        val doc: Document
+        try {
+            doc = Jsoup.connect("$wiki$url").get()  // <2>
+        }catch (e: Exception){
+            return null
+        }
+
+        val movie = Movie() // <3>
+        doc.select(".infobox tr")   // <4>
+            .forEach { ele ->   // <5>
+                when {
+                    ele.getElementsByTag("th")?.hasClass("summary") ?: false -> {   // <6>
+                        movie.title = ele.getElementsByTag("th")?.text()
+                    }
+                    else -> {
+                        val value: String? = if (ele.getElementsByTag("li").size > 1)
+                            ele.getElementsByTag("li")
+                                .map(Element::text)
+                                .filter(String::isNotEmpty)
+                                .joinToString(", ") else
+                            ele.getElementsByTag("td")?.first()?.text() // <7>
+
+                        when (ele.getElementsByTag("th")?.first()?.text()) {    // <8>
+                            "Directed by" -> movie.directedBy = value ?: ""
+                            "Produced by" -> movie.producedBy = value ?: ""
+                            "Written by" -> movie.writtenBy = value ?: ""
+                            "Starring" -> movie.starring = value ?: ""
+                            "Music by" -> movie.musicBy = value ?: ""
+                            "Release date" -> movie.releaseDate = value ?: ""
+                            "title" -> movie.title = value ?: ""
+                        }
+                    }
+                }
+            }
+        return movie
+    }
+}
